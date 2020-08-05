bde45bbcd5d78b27b38e3367141476757d1d3f24
The Unfit Donkey <bjc2dogs@gmail.com>
2020-08-04 20:16:02 -0700

    Fix for undefined output error

--- HackerGenerator.js
+++ HackerGenerator.js
@@ -4,6 +4,7 @@ class Line {
      * @return {Line} New line
      */
     constructor() {
+        this.output="";
         //Generate input and output
         let a=Math.floor(Math.random()*55);
         if(a==0) {//HIDE
